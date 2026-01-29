9d8f54cc9fb35eb57c42e38192520b8bf178d79e
Cytus1 <xyloaex@gmail.com>
2026-01-29 12:15:37 -0500

    working with layout1

--- app/src/main/java/edu/temple/classactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/classactivity/MainActivity.kt
@@ -1,4 +1,7 @@
 package edu.temple.classactivity
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -9,6 +12,17 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+
+        val button = findViewById<Button>(R.id.button)
+        val editText = findViewById<EditText>(R.id.editTextText)
+        val textView = findViewById<TextView>(R.id.textView)
+
+        button.setOnClickListener {
+            textView.text = editText.text.toString()
+        }
+
     }
 }
